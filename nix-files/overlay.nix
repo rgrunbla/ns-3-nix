d9cb7b55bc66fb56bd98e431c1f658bf3eb3eee7
Rémy Grünblatt <remy@grunblatt.org>
2021-03-04 21:45:53 +0100

    Mise à jour des scripts de compilation

--- nix-files/overlay.nix
+++ nix-files/overlay.nix
@@ -5,7 +5,8 @@ let
   lib = self.pkgs.lib;
   stdenv = self.pkgs.stdenv;
   pythonEnv = super.python3;
-  in {
+in
+{
   ns-3 = super.ns-3.override {
     stdenv = super.ccacheStdenv.override {
       extraConfig = ''
@@ -15,13 +16,13 @@ let
       '';
     };
   };
-  
+
   my-ns-3 = self.ns-3.overrideAttrs
     (oldAttrs: rec {
       src = /home/remy/ns-3/ns-3;
       patches = [ ];
-      # doCheck = false;
-      checkPhase =  ''
+      doCheck = false;
+      checkPhase = ''
         ${pythonEnv.interpreter} ./test.py --nowaf --verbose
       '';
     });
--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 #!/usr/bin/env nix-shell
 with import <nixpkgs> { overlays = [ (import ./nix-files/overlay.nix) ]; };
-stdenv.mkDerivation {
-  name = "ns-3";
-  buildInput = [ my-ns-3 ];
+
+mkShell {
+  nativeBuildInputs = [
+    llvmPackages_latest.clang
+    pkg-config
+  ];
+  buildInputs = [ my-ns-3 ];
 }
